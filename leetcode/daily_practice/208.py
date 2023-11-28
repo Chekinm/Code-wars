34e0a04791738b14be9a0539cf7a85ab019c334a
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-11-28 16:01:01 +0200

    20 solved

---
+++ leetcode/daily_practice/208.py
@@ -0,0 +1,44 @@
+class TrieObj:
+
+    def __init__(self, letter):
+        self.value = letter
+        self.childs = {}
+        self.has_elem = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.head = TrieObj(None) 
+
+
+    def insert(self, word):
+        curr_node = self.head
+        for letter in word:
+            if letter in curr_node.childs:
+                curr_node = curr_node.childs[letter]
+            else:
+                curr_node.childs[letter] = TrieObj(letter)
+                curr_node = curr_node.childs[letter]
+        curr_node.has_elem = True   
+
+    def search(self, word):
+        curr_node = self.head
+        for letter in word:
+            if letter in curr_node.childs:
+                curr_node = curr_node.childs[letter]
+            else:
+                return False
+        if curr_node.has_elem:
+            return True
+        return False
+
+    def startsWith(self, startsWith):
+        curr_node = self.head
+        for letter in startsWith:
+            if letter in curr_node.childs:
+                curr_node = curr_node.childs[letter]
+            else:
+                return False
+        return True
+    
