73e1a1ce1c41a30a503ffed2996d56160a0eadc9
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-01-16 17:56:10 +0200

    17

---
+++ leetcode/daily_practice/17.py
@@ -0,0 +1,14 @@
+import itertools as it
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        map_dict={
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz',
+        }
+        return list(''.join(i) for i in it.product(*[map_dict[dig] for dig in digits])) if len(digits)>0 else []
