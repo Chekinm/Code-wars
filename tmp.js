80004512d03a7b09223c0d2406d2a4e47f31849a
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-01-19 19:13:06 +0200

    js

---
+++ tmp.js
@@ -0,0 +1,17 @@
+var undefined = 2
+console.log(undefined)
+var a
+console.log(a)
+console.log(undefined)
+var a = undefined
+console.log(a)
+b = []
+b[5] = 100
+console.log(b[1])
+c = b[1] + 3
+console.log(c) 
+
+console.log(true)
+
+
+
