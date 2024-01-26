865e0f643c03f2133390d354e84bba30e0b458e8
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-01-26 12:14:59 +0200

    pep8

--- leetcode/daily_practice/1829.py
+++ leetcode/daily_practice/1829.py
@@ -1,10 +1,11 @@
 from functools import reduce
 from operator import xor
-from collections import deque
+
+
 class Solution:
-    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]: 
-        res = [reduce(xor, nums)^((1 << maximumBit) - 1)]
+
+    def getMaximumXor(self, nums: list[int], maximumBit: int) -> list[int]:
+        res = [reduce(xor, nums) ^ ((1 << maximumBit) - 1)]
         for i in range(1, len(nums)):
-            res.append(res[i-1]^nums[-1*i])
+            res.append(res[i - 1] ^ nums[-1 * i])
         return res
-        
