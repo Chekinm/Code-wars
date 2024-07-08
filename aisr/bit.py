f8759dcf8ac78b75129c71875d904ebdf073f658
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-07-08 19:52:52 +0300

    make some order in folder

--- aisr/bit.py
+++
@@ -1,9 +0,0 @@
-def find_unique(nums: list[int]) -> int:
-    
-    res = 0
-    for num in nums:
-        res ^= num
-    return res
-
-print(find_unique([1,1,2,2,100]))
-
--- tmp.js
+++
@@ -1,9 +0,0 @@
-class Person = function (nums){
-    this.nums = nums
-}
-let Person.prototype.toString = function(){
-    return('Hello', nums)
-}
-
-n = new Person([1,2,3])
-console.log(n)
